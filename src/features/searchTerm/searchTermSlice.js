f40074dbb93d91384c2e12a9a87312b398d06e1b
Simona Pizio <simona.pizio@gmail.com>
2022-03-30 17:46:23 +0200

    Update searchTermSlice feature Redux

--- src/features/searchTerm/searchTermSlice.js
+++ src/features/searchTerm/searchTermSlice.js
@@ -1,1 +1,29 @@
+//action setSearchTerm and clearSearchTerm
 
+import { SearchTerm } from "./SearchTerm";
+
+const initialState = '';
+
+export function setSearchTerm (input) {
+  return {
+    type: 'searchTerm/setSearchTerm',
+    payload: input
+  };
+};
+
+export function clearSearchTerm () {
+  return {
+    type: 'searchTerm/clearSearchTerm'
+  };
+};
+
+export const searchTermReducer = (state= initialState, action) => {
+  switch (action.type) {
+    case 'searchTerm/setSearchTerm':
+      return action.payload;
+    case 'searchTerm/clearSearchTerm':
+      return '';
+    default:
+      return state;
+  };
+};
