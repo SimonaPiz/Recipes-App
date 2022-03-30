e4c32092be9f55c6db11c28c06a029c018a985e4
Simona Pizio <simona.pizio@gmail.com>
2022-03-30 17:23:12 +0200

    Update allRecipesSlice feature Redux

--- src/features/allRecipes/allRecipesSlice.js
+++ src/features/allRecipes/allRecipesSlice.js
@@ -1,1 +1,24 @@
+import allRecipesData from '../../../data';
 
+//action loadData
+export const loadData = () => {
+  return {
+    type: 'allRecipes/loadData',
+    payload: allRecipesData
+  }
+};
+
+const initialState = [];
+
+export const allRecipesReducer = (state= initialState, action) => {
+  switch (action.type) {
+    case 'allRecipes/loadData':
+      return action.payload;
+    case 'favoriteRecipes/addRecipe':
+      return allRecipesData.filter(recipe => recipe.id !== action.payload.id);
+    case 'favoriteRecipes/revomeRecipes':
+      return [...allRecipesData, action.payload];
+    default: 
+      return allRecipes;
+  }
+};
