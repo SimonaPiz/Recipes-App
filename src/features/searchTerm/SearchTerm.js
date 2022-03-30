c6a24b2ee3eb62e47698e9c20cd0c4998ba531a1
Simona Pizio <simona.pizio@gmail.com>
2022-03-30 14:33:52 +0200

    Update SearchTerm component react

--- src/features/searchTerm/SearchTerm.js
+++ src/features/searchTerm/SearchTerm.js
@@ -1,1 +1,42 @@
+import React from 'react';
+const searchIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/search.svg'
+const clearIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/clear.svg'
 
+//import setSearchTerm and clearSearchTerm
+
+export const SearchTerm = (props) => {
+  const {searchTerm, dispatch} = props;
+
+  //listener onSearchTermChange
+  const onSearchTermChangeHandler = ({target}) => {
+    const userInput = target.value;
+    dispatch(setSearchTerm(userInput));
+  };
+
+  //listener onClearSearchTerm
+  const onClearSearchTermHandler = () => {
+    dispatch(clearSearchTerm());
+  };
+
+  return (
+    <div id='search-container'>
+      <img id='search-icon' alt='' src={searchIconUrl} />
+      <input 
+        id='search'
+        type='text'
+        value={searchTerm}
+        onChange={onSearchTermChangeHandler}
+        placeholder='Search recipes'
+      />
+      {searchTerm.length > 0 && (
+        <button
+          onClick={onClearSearchTermHandler}
+          type='button'
+          id='search-clear-button'
+        >
+          <img src={clearIconUrl} alt='' />
+        </button>
+      )}
+    </div>
+  );
+};
