a3c615998f5bb4b480e238a85de04b27e65bee8e
Simona Pizio <simona.pizio@gmail.com>
2022-03-30 13:59:34 +0200

    Update AllRecipes component react

--- src/features/allRecipes/AllRecipes.js
+++ src/features/allRecipes/AllRecipes.js
@@ -1,14 +1,34 @@
 import React, { useEffect } from 'react';
 import Recipe from "../../components/Recipe";
+import FavoriteButton from '../../components/FavoriteButton';
+
+//import loadData and addRecipe
 
 const favoriteIconURL = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/favorite.svg'
 
 export const AllRecipes = (props) => {
+  const {allRecipes, dispatch} = props;
+
+  const onFirstRender = () => {
+    dispatch(loadData());
+  };
+
+  useEffect(onFirstRender,[]);
+
+  const onAddRecipeHandler = (recipe) => {
+    dispatch(addRecipe(recipe));
+  };
+
   return (
     <div className="recipes-container">
       {allRecipes.map((recipe) => (
         <Recipe recipe={recipe} key={recipe.id}>
-          
+          <FavoriteButton 
+            onClickHandler={() => onAddRecipeHandler(recipe)}
+            icon={favoriteIconURL}
+          >
+            Add to Favorites
+          </FavoriteButton>
         </Recipe>
       ))}
     </div>
