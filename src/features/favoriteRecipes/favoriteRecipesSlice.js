ba1572c0f66f06fcbfc4172589b992339cefc2e1
Simona Pizio <simona.pizio@gmail.com>
2022-04-01 16:42:32 +0200

    Correct favoriteRecipesSlice.js, export Selector functions

--- src/features/favoriteRecipes/favoriteRecipesSlice.js
+++ src/features/favoriteRecipes/favoriteRecipesSlice.js
@@ -30,3 +30,14 @@ export function removeRecipe(recipe) {
     payload: recipe
   }
 }
+
+export const selectFavoriteRecipes = (state) => state.favoriteRecipes;
+
+export const selectFilteredFavoriteRecipes = (state) => {
+  const favoriteRecipes = selectFavoriteRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return favoriteRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
