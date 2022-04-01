cb79f44bc56f7dec050f85640ab6681c663d26c9
Simona Pizio <simona.pizio@gmail.com>
2022-04-01 17:00:50 +0200

    Correct favoriteRecipes.js, Set state and dipastch with hooks useSelector and useDispatch

--- src/features/favoriteRecipes/FavoriteRecipes.js
+++ src/features/favoriteRecipes/FavoriteRecipes.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import FavoriteButton from '../../components/FavoriteButton';
 import Recipe from '../../components/Recipe';
 
 //import removeRecipe
-import { removeRecipe } from './favoriteRecipesSlice';
+import { removeRecipe, selectFilteredFavoriteRecipes } from './favoriteRecipesSlice';
 
 const unfavoriteIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/unfavorite.svg'
 
-export const FavoriteRecipes = (props) =>{
-  const {favoriteRecipes, dispatch} = props;
+export const FavoriteRecipes = () =>{
+  const favoriteRecipes = useSelector(selectFilteredFavoriteRecipes);
+  const dispatch = useDispatch();
 
   const onRemoveRecipeHandler = (recipe) => {
     dispatch(removeRecipe(recipe));
